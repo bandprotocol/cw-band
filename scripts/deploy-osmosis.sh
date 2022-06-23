3b1acee3ba1058465e09ce904d13ee310c29a7ca
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2022-06-23 22:58:47 +0700

    add script for deploy to osmosis testnet

---
+++ scripts/deploy-osmosis.sh
@@ -0,0 +1,18 @@
+# To create account
+## run "osmosisd keys add test --recover". Then, paste mnemonic
+
+# Prepare 
+CHAIN_ID=osmo-test-4
+DEPLOYER=test
+
+# Build contract
+cargo run-script optimize
+
+TX=$(osmosisd tx wasm store ./artifacts/poc_price_feed.wasm  --from $DEPLOYER --chain-id=$CHAIN_ID --gas-prices 0.1uosmo --gas auto --gas-adjustment 1.3 -b block --output json -y | jq -r '.txhash')
+CODE_ID=$(osmosisd query tx $TX --output json | jq -r '.logs[0].events[-1].attributes[0].value')
+echo "Your contract code_id is $CODE_ID"
+
+INITIAL_STATE='{}'
+osmosisd tx wasm instantiate $CODE_ID $INITIAL_STATE --amount 50000uosmo  --label "Counter Contract" --from $DEPLOYER --chain-id $CHAIN_ID --gas-prices 0.1uosmo --gas auto --gas-adjustment 1.3 -b block -y --no-admin
+CONTRACT_ADDR=$(osmosisd query wasm list-contract-by-code $CODE_ID --output json | jq -r '.contracts[0]')
+echo "Your contract address is $CONTRACT_ADDR"
