5e600bba6ec81f2e8503475620cafb4ef6f95f44
taobun <bun@bandprotocol.com>
2024-04-29 16:29:07 +0700

    bump optimizer version

--- scripts/build_artifacts.sh
+++ scripts/build_artifacts.sh
@@ -4,4 +4,4 @@ set -o pipefail
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.13.0
+  cosmwasm/workspace-optimizer:0.15.1
