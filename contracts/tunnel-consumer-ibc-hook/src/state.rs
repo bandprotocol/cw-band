f74d13feae2e5e8e990c21c466af13ce415d6a32
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2025-05-19 14:08:53 +0700

    changed namespace for sender

--- contracts/tunnel-consumer-ibc-hook/src/state.rs
+++ contracts/tunnel-consumer-ibc-hook/src/state.rs
@@ -4,5 +4,5 @@ use cw_controllers::Admin;
 use cw_storage_plus::Map;
 
 pub const ADMIN: Admin = Admin::new("admin");
-pub const SENDERS: Map<Addr, ()> = Map::new("tunnel_config");
+pub const SENDERS: Map<Addr, ()> = Map::new("senders");
 pub const SIGNAL_PRICE: Map<&str, Price> = Map::new("signal_price");
